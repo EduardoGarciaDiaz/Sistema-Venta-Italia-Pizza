7a9c35c88ad738cb7bb1fc9f6e7293c502c47648
TristanSantiago1 <tristan.santiago520@gmail.com>
2024-03-13 21:41:35 -0600

    Interfaz Registro Usuario

---
+++ ItaliaPizza-Cliente/Vistas/RegistroUsuario.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ItaliaPizza_Cliente.Vistas
+{
+    /// <summary>
+    /// Interaction logic for RegistroUsuario.xaml
+    /// </summary>
+    public partial class RegistroUsuario : Page
+    {
+        public RegistroUsuario()
+        {
+            InitializeComponent();
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BttGuardarUsuario_Click(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
