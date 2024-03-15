8f37f8a2b33d539afe0151c1da457c77708f0c77
Eduardo García Díaz <eduardo.gd17@gmail.com>
2024-03-14 19:02:10 -0600

    ConvertidorBytes

---
+++ ItaliaPizza-Servidor/ItaliaPizza-Contratos/DTOs/Receta.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItaliaPizza_Contratos.DTOs
+{
+    [DataContract]
+    public class Receta
+    {
+        [DataMember]
+        public string Codigo { get; set; }
+
+        [DataMember]
+        public string Nombre { get; set; }
+
+        [DataMember]
+        public Byte[] FotoProducto { get; set; }
+    }
+}
--- ItaliaPizza-Servidor/ItaliaPizza-Servicios/Auxiliares/AuxiliarPreparacionDatos.cs
+++ ItaliaPizza-Servidor/ItaliaPizza-Servicios/Auxiliares/AuxiliarPreparacionDatos.cs
@@ -8,12 +8,14 @@ using System.Threading.Tasks;
 
 namespace ItaliaPizza_Servicios.Auxiliares
 {
-    public static class AuxiliarUnificacionDatos
+    public static class AuxiliarPreparacionDatos
     {
-        public static List<Categoria> UnificarListaCategorias(List<Categoria> categorias,
+        public static List<Categoria> PrepararListaCategorias(
             List<CategoriasProductoVenta> categoriasProductoVenta,
             List<CategoriasInsumo> categoriasInsumo)
         {
+            List<Categoria> categorias = new List<Categoria>();
+
             categorias.AddRange(categoriasProductoVenta.Select(categoriaProductoVenta => new Categoria
             {
                 Id = categoriaProductoVenta.IdCategoriaProductoVenta,
