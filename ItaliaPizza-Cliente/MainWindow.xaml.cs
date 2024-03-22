22393da67c9d687877e6caa49f337b10ba4a14e1
TristanSantiago1 <tristan.santiago520@gmail.com>
2024-03-21 19:11:18 -0600

    Opciones cajero

--- ItaliaPizza-Cliente/MainWindow.xaml.cs
+++ ItaliaPizza-Cliente/MainWindow.xaml.cs
@@ -67,7 +67,6 @@ namespace ItaliaPizza_Cliente
         {
             AgregarBotonPedido();
             AgregarBotonPedidos();
-            AgregarBotonRegistrarUsuarios();
         }
 
         private void OpcionesPanelChef()
